ffc8af65f0229f3c872a92868c73b7d49fa5b49a
serhattastan <serhattastannn@gmail.com>
2024-09-20 03:51:29 +0300

    Fix: Implemented OnBackInvokedCallback for Android 13+ back button handling in BaseFragment
    - Added OnBackInvokedCallback for Android 13+ devices in BaseFragment
    - Continued using OnBackPressedDispatcher for Android versions below 13
    - Managed back button behavior using NavController for fragment navigation
    - Resolved log warnings related to back button handling on API 33+

--- app/src/main/java/com/dijitalgaraj/study/base/BaseFragment.kt
+++ app/src/main/java/com/dijitalgaraj/study/base/BaseFragment.kt
@@ -1,9 +1,11 @@
 package com.dijitalgaraj.study.base
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.window.OnBackInvokedDispatcher
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
@@ -12,6 +14,8 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
+import androidx.activity.OnBackPressedCallback
+import androidx.navigation.fragment.findNavController
 
 abstract class BaseFragment<out ViewModel : BaseViewModel<BaseActionState>, Binding : ViewBinding>(
     viewModelClass: Class<ViewModel>
@@ -21,7 +25,6 @@ abstract class BaseFragment<out ViewModel : BaseViewModel<BaseActionState>, Bind
     val binding get() = _binding!!
 
     abstract val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> Binding
-
 
     protected val viewModel: ViewModel by lazy {
         ViewModelProvider(this)[viewModelClass]
@@ -40,18 +43,49 @@ abstract class BaseFragment<out ViewModel : BaseViewModel<BaseActionState>, Bind
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // Geri tuşunu yönet
+        handleBackButton()
+
         with(viewModel) {
             actionStateJob =
                 actionState.map { it.getContentIfNotHandled() }.onEach(::renderActionState)
                     .launchIn(lifecycleScope)
         }
-            init()
+        init()
     }
 
     open fun init() {}
 
     abstract fun renderActionState(actionState: BaseActionState?)
 
+    private fun handleBackButton() {
+        // Android 13 ve üstü için OnBackInvokedCallback kullanıyoruz
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            requireActivity().onBackInvokedDispatcher.registerOnBackInvokedCallback(
+                OnBackInvokedDispatcher.PRIORITY_DEFAULT
+            ) {
+                onBackPressed() // Geri tuşu yönetimi
+            }
+        } else {
+            // Eski Android sürümleri için onBackPressedDispatcher kullanıyoruz
+            requireActivity().onBackPressedDispatcher.addCallback(
+                viewLifecycleOwner,
+                object : OnBackPressedCallback(true) {
+                    override fun handleOnBackPressed() {
+                        onBackPressed()
+                    }
+                }
+            )
+        }
+    }
+
+    open fun onBackPressed() {
+        val navController = findNavController()
+        if (!navController.popBackStack()) {
+            // Geri yığındaki son fragment ise, activity'yi sonlandır
+            requireActivity().finish()
+        }
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
